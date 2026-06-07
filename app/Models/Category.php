0c15cd07c4275cbfad9f5dfb9c31ffc951887a7a
llrdhann <adhann30@gmail.com>
2026-06-07 10:30:28 +0700

    menambah relasi one to many untuk category dan product

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\hasMany;
 
 class Category extends Model
 {
     //
-    
+    use hasFactory;
+    protected $fillable = ['name'];
+    public function products(): hasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\Hasfactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Darabase\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-   
+    //
+    use Hasfactory;
+    protected $fillable = [
+        'category_id',
+        'name',
+        'stock',
+        'cost_price',
+        'selling_price'
+    ];
+    // or
+    // protected $guarded = ['id'];
+
+    public function category(): BelongsTo
+    {
+        return $this->BelongsTo(Category::class);
+    }
 }
